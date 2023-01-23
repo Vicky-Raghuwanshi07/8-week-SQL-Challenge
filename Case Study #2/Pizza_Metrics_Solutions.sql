9e3ba981f82dfffa9e5cd19b0f563980a73da801
Vicky Raghuwanshi <74697810+Vicky-Raghuwanshi07@users.noreply.github.com>
2023-01-23 20:57:17 +0530

    Update Pizza_Metrics_Solutions.sql
    Added solution of 3rd question

--- Case Study #2/Pizza_Metrics_Solutions.sql
+++ Case Study #2/Pizza_Metrics_Solutions.sql
@@ -8,3 +8,13 @@ FROM #cleaned_customer_orders;
 
 SELECT count(distinct order_id) as unique_orders
 FROM #cleaned_customer_orders;
+
+
+-- 3. How many successful orders were delivered by each runner?
+
+SELECT 
+    runner_id,
+    count(order_id) as successful_orders
+FROM #cleaned_runner_orders
+WHERE cancellation is null
+GROUP BY runner_id;
