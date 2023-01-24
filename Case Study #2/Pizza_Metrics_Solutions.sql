d1065e6cb3c164d41435c11fef676aaaea639b02
Vicky Raghuwanshi <74697810+Vicky-Raghuwanshi07@users.noreply.github.com>
2023-01-24 20:30:19 +0530

    Update Pizza_Metrics_Solutions.sql

--- Case Study #2/Pizza_Metrics_Solutions.sql
+++ Case Study #2/Pizza_Metrics_Solutions.sql
@@ -18,3 +18,37 @@ SELECT
 FROM #cleaned_runner_orders
 WHERE cancellation is null
 GROUP BY runner_id;
+
+
+-- 4. How many of each type of pizza was delivered?
+
+SELECT
+	p.pizza_name,
+	COUNT(c.order_id) as delivered
+FROM 
+	#cleaned_customer_orders c,
+	#cleaned_runner_orders r,
+	pizza_names p
+WHERE
+	c.order_id = r.order_id
+	AND
+	c.pizza_id = p.pizza_id
+	AND
+	r.cancellation is null
+GROUP BY p.pizza_name;
+
+
+-- 5.How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT
+	customer_id,
+	pizza_name,
+	COUNT(c.order_id) as ordered
+FROM 
+	#cleaned_customer_orders c,
+	pizza_names p
+WHERE c.pizza_id = p.pizza_id
+GROUP BY 
+	customer_id,
+	pizza_name
+ORDER BY customer_id;
